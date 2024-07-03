d17281dd9238b022d9862a9a9924634439101f26
PBX1-BOTS <bxjjjzid@gmail.com>
2024-07-03 08:14:38 +0530

    Update config.py

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv("config.env")
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("20841292"))
+API_HASH = getenv("58c4e84b9b2506684df6447d3e324dd9")
+BOT_TOKEN = getenv("7451705797:AAEu-w-v4M-hyrYzM_tUCER4ueW_ZAdm520")
 
-SUDO_USERID = json.loads(getenv("SUDO_USERID"))
-AUTHORIZED_CHATS = json.loads(getenv("AUTHORIZED_CHATS"))
+SUDO_USERID = json.loads(getenv("6052303737"))
+AUTHORIZED_CHATS = json.loads(getenv("0"))
